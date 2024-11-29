38f35e3abe82d8a7496b8b0ac31e48d44e58a39e
ShaneBeee <shanebolenback@me.com>
2024-11-28 17:15:26 -0800

    EffParticle - fix pattern

--- src/main/java/com/shanebeestudios/skbee/elements/particle/effects/EffParticle.java
+++ src/main/java/com/shanebeestudios/skbee/elements/particle/effects/EffParticle.java
@@ -45,8 +45,9 @@ public class EffParticle extends Effect {
 
     static {
         Skript.registerEffect(EffParticle.class,
-            "(lerp|draw|make) %number% [of] %particle% [particle] [using %-itemtype/blockdata/dustoption/dusttransition/vibration" +
-                "/number/color/bukkitcolor%] %directions% %locations% [with (delta|offset) %-vector%] [with extra %-number%] [(1:with force)] [(for|to) %-players%]");
+            "(lerp|draw|make) %number% [of] %particle% [particle] " +
+                "[using %-material/itemstack/blockdata/dustoption/dusttransition/vibration/number/color/bukkitcolor%] " +
+                "%directions% %locations% [with (delta|offset) %-vector%] [with extra %-number%] [(1:with force)] [(for|to) %-players%]");
     }
 
     private Expression<Number> count;
